3a890b61d4de181b8fddb6621b7eb90468c9a68f
Hack-Light <onohsomtochukwubasil@gmail.com>
2022-10-28 16:18:38 +0100

    fix

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+app.use(cors())
+
 app.get("/", (req, res) => {
   res.json({
     slackUsername: "Light",
