704589320aeff9f1160199fbf2d7a68adc3f7bea
Hack-Light <onohsomtochukwubasil@gmail.com>
2022-11-03 12:32:33 +0100

    code done

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const port = process.env.PORT || 3000;
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false}));
-app.use(cors())
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.json({
